a975e0be67f107637e61ed7bc6e06a3cb79acb8c
Sergey Evtushenko <evtushru208@gmail.com>
2022-09-30 22:55:08 +0200

    создал переменные и удалил выводы в консоль

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
-const b = "наша ветка lesson01";
-
-const logged = () => {
-	console.log('lesson02');
-
-};
-const logged1 = () => {
-	console.log('lesson03');
-
-};
-const logged2 = () => {
-	console.log('hello git hub');
-
-};
+const a = 1;
+let b = "вторая переменная";
